9577c785af72df77d30a7a17cfcbbef9b5a7b0c7
devdariill <93483481+devdariill@users.noreply.github.com>
2023-10-07 09:47:49 +0000

    _

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -72,17 +72,6 @@ const SongControl = ({ audio }) => {
     <div className="flex gap-x-3 text-xs pt-2">
       <span className="opacity-50 w-12 text-right">{formatTime(currentTime)}</span>
 
-      <Slider
-        value={[currentTime]}
-        max={audio?.current?.duration ?? 0}
-        min={0}
-        className="w-[400px]"
-        onValueChange={(value) => {
-          const [newCurrentTime] = value
-          audio.current.currentTime = newCurrentTime
-        }}
-      />
-
       <span className="opacity-50 w-12">
         {duration ? formatTime(duration) : '0:00'}
       </span>
